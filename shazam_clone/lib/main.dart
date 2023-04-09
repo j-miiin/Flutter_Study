6f24aaeffbdbc8cef85e163a5a3a38e4919bce11
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-04-09 19:21:00 +0900

    2주차 과제 작성 완료

--- shazam_clone/lib/main.dart
+++ shazam_clone/lib/main.dart
@@ -118,7 +118,171 @@ class FirstTab extends StatelessWidget {
       },
     ];
 
-    return Center(child: Text('첫번째 페이지'));
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment
+                .start, // 아이콘에 padding을 줘도 라이브러리 텍스트는 내려가지 않도록 함
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Icon(Icons.settings),
+              ),
+              Text(
+                "라이브러리",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Icon(null), // 글씨를 가운데로 오게 하기 위해 빈 아이콘을 넣어줌
+            ],
+          ),
+          SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Row(
+              children: [
+                ImageIcon(
+                  NetworkImage("https://i.ibb.co/hxNbZ8p/shazam.png"),
+                  size: 18,
+                ),
+                SizedBox(width: 12),
+                Text(
+                  "Shazam",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Row(
+              children: [
+                Icon(Icons.person_rounded),
+                SizedBox(width: 12),
+                Text(
+                  "아티스트",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Row(
+              children: [
+                Icon(Icons.music_note),
+                SizedBox(width: 12),
+                Text(
+                  "회원님을 위한 재생 목록",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          SizedBox(height: 16),
+          Text(
+            "최근 Shazam",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          SizedBox(height: 16),
+          Expanded(
+            // ListView.builder처럼 Column 안에 넣으려면 Expanded로 감싸줘야 함
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 5,
+              ),
+              itemCount: songs.length,
+              itemBuilder: ((context, index) {
+                var song = songs[index];
+                String imageUrl = song['imageUrl']!;
+                String title = song['title']!;
+                String artist = song['artist']!;
+
+                return Container(
+                  margin: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          blurRadius: 1,
+                          spreadRadius: 1,
+                        )
+                      ]),
+                  child: Column(children: [
+                    // Container에 border를 줘도 이미지는 잘리지 않기 때문에 ClipRRect를 사용
+                    ClipRRect(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(8),
+                            topRight: Radius.circular(8)),
+                        child: Image.network(
+                          imageUrl,
+                          fit: BoxFit.cover,
+                          height: MediaQuery.of(context)
+                                  .size
+                                  .width * // 이미지가 grid 폭에 맞춰서 변하기 위해
+                              0.5 *
+                              5 /
+                              3 *
+                              0.55, // grid에서 55% 정도 높이를 차지
+                        )),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                title,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                artist,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                              Spacer(),
+                              Image.network(
+                                "https://i.ibb.co/KG9m5QS/applemusic.png",
+                                width: 60,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ]),
+                );
+              }),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
 
@@ -133,10 +297,107 @@ class SecondTab extends StatelessWidget {
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          colors: [Colors.blue[300]!, Colors.blue[900]!],
+          colors: [
+            Colors.blue[300]!,
+            Colors.blue[900]!
+          ], // null 값이 아니므로 뒤에 느낌표 붙이면 됨
         ),
       ),
-      child: SafeArea(child: Text("hello")),
+      child: SafeArea(
+        // SafeArea를 사용하면 아이폰 윗 부분 노치, 아래 홈버튼 등을 피해서 화면을 만들 수 있음
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween, // 버튼 사이에 Spacer()를 둬도 됨
+              children: [
+                // IconButton 자체에는 자체 padding이 있음
+                // -> Icon과 Text를 붙이기 위해서 GestureDetector로 Column을 묶음
+                GestureDetector(
+                  onTap: () {
+                    DefaultTabController.of(context)
+                        .animateTo(0); // 지금 여기서 0번째로 이동해라
+                  },
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.person,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "라이브러리",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    DefaultTabController.of(context).animateTo(2);
+                  },
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.show_chart,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "차트",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+              height: MediaQuery.of(context).size.height *
+                  0.1 // 전체 화면 길이의 10% 만큼의 공백
+              ),
+          Text(
+            "Shazam하려면 탭하세요",
+            style: TextStyle(
+                color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.06),
+          Container(
+            alignment: Alignment.center,
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.blue[300],
+              shape: BoxShape.circle,
+            ),
+            child: Image.network(
+              "https://i.ibb.co/hxNbZ8p/shazam.png",
+              color: Colors.white,
+              width: 130,
+              height: 130,
+            ),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.12),
+          Container(
+            width: 50,
+            height: 50,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.blue[400],
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              Icons.search,
+              color: Colors.white,
+              size: 30,
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
@@ -201,6 +462,207 @@ class ThirdTab extends StatelessWidget {
       ],
     };
 
-    return Center(child: Text('세번째 페이지'));
+    return SafeArea(
+        child: Column(
+      children: [
+        Text(
+          "차트",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+        ),
+        SizedBox(height: 18),
+        Expanded(
+          child: ListView(
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 180,
+                    color: Colors.purple[900],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width *
+                            0.8, // 기기 폭에 따라 버튼 폭이 달라짐
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                Colors.white), // 그냥 Colors.white라고 쓰면 안됨
+                          ),
+                          child: Text(
+                            "국가 및 도시별 차트",
+                            style: TextStyle(
+                              color: Colors.purple[900],
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "전 세계",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                height: 8,
+                color: Colors.grey[400],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(
+                      "대한민국 차트",
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      "모두 보기",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              // map 자체는 이미 list를 반환함
+              Row(
+                children: chartData['korea']!.map((element) {
+                  String imageUrl = element['imageUrl']!;
+                  String name = element['name']!;
+                  String artist = element['artist']!;
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(
+                          imageUrl,
+                          width: MediaQuery.of(context).size.width * 0.29,
+                        ),
+                        Text(
+                          name,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(artist),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+              Container(
+                width: double.infinity,
+                height: 8,
+                color: Colors.grey[400],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(
+                      "글로벌 차트",
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      "모두 보기",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                children: chartData['global']!.map((element) {
+                  String imageUrl = element['imageUrl']!;
+                  String name = element['name']!;
+                  String artist = element['artist']!;
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(
+                          imageUrl,
+                          width: MediaQuery.of(context).size.width * 0.29,
+                        ),
+                        Text(
+                          name,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(artist),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+              Container(
+                width: double.infinity,
+                height: 8,
+                color: Colors.grey[400],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(
+                      "뉴욕 차트",
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      "모두 보기",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                children: chartData['newyork']!.map((element) {
+                  String imageUrl = element['imageUrl']!;
+                  String name = element['name']!;
+                  String artist = element['artist']!;
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(
+                          imageUrl,
+                          width: MediaQuery.of(context).size.width * 0.29,
+                        ),
+                        Text(
+                          name,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(artist),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              )
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
